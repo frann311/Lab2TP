8d99132ffd04eb2205e8eff6d26b58ac77631e54
frann311 <avgustinfrancisco1@gmail.com>
2024-11-13 18:19:19 -0300

    conexion a clever

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "consultas",
+  host: "bphshosknse2qu8m8ml3-mysql.services.clever-cloud.com",
+  user: "uqjg30curaeyxn4s",
+  password: "ErFXcvM3NaieEYlnaPaE",
+  database: "bphshosknse2qu8m8ml3",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
